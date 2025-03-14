0f953d792a9a7d1e17303bf835321327b2fe221e
Brendan Irvine-Broque <birvine-broque@cloudflare.com>
2025-03-14 12:33:19 -0700

    Simple example

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		request.signal.onabort = () => {
+			console.log('The request was aborted!');
+		  };
+	  
+		  const { readable, writable } = new IdentityTransformStream();
+		  ctx.waitUntil(sendPing(writable));
+		  return new Response(readable);
 	},
 } satisfies ExportedHandler<Env>;
+
+
+async function sendPing(writable: WritableStream): Promise<void> {
+	const writer = writable.getWriter();
+	const enc = new TextEncoder();
+
+	for (;;) {
+	  // Send 'ping' every second to keep the connection alive
+	  await writer.write(enc.encode('ping')); 
+	  scheduler.wait(1000);
+	}
+ }
